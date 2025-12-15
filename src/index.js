649117f1ec12753b62696fe365ccdced5b96b2ca
VanillaTank <tedien2016@yandex.ru>
2025-12-16 00:52:13 +0800

    api

--- src/index.js
+++ src/index.js
@@ -7,25 +7,21 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-// Todo настроить, когда будет сервер
-// const whitelist = [
-//   'http://127.0.0.1:8080',
-//   'http://localhost:8080',
-//   'http://162.250.125.132',
-//   'http://affystat.durban',
-// ]
-//
-// const corsOptions = {
-//   // @ts-ignore
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     }
-//   }
-// }
-// app.use(cors(corsOptions))
+const whitelist = [
+  'http://127.0.0.1:8080',
+  'http://localhost:8080',
+  'http://med-tests.fvds.ru',
+  process.env.SERVER_IP,
+]
 
-app.use(cors())
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    }
+  }
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.post('/api/login', login)
