0eaceb0dbdbeb1c54a62364441151bbebc445df6
VanillaTank <tedien2016@yandex.ru>
2025-12-17 00:35:15 +0800

    cors

--- src/index.js
+++ src/index.js
@@ -7,36 +7,9 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-// app.enable('trust proxy')
-
-const whitelist = [
-  'http://localhost:5173',
-  'http://med-tests.fvds.ru',
-]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true)
-    }
-  }
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors())
 }
-app.use(cors(corsOptions))
-
-
-app.use((req, res, next) => {
-  console.log('req.ip', req.ip)
-  // if (!req.headers.origin) {
-  //   const clientIp = req.ip;
-  //
-  //   console.log('clientIp', clientIp)
-    // Можно добавить дополнительную проверку IP
-    // const allowedIps = ['trusted-ip-1', 'trusted-ip-2'];
-    // if (!allowedIps.includes(clientIp)) {
-    //   return res.status(403).send('Forbidden');
-    // }
-  // }
-  next();
-})
 
 app.use(express.json())
 
