b8bbab9ca7c74338252358c70b5beefa5eb4103f
tanya <tedien2016@yandex.ru>
2026-09-02 22:57:52 +0400

    переименование колонки в базе

--- src/db/db-action.js
+++ src/db/db-action.js
@@ -2,15 +2,15 @@ const db = require('./db-init.js')
 
 async function doAction() {
   try {
-    await db.prepare('ALTER TABLE tests ADD COLUMN isShowAverage INTEGER DEFAULT 0').run()
-    console.log('Колонка успешно добавлена')
+    // await db.prepare('ALTER TABLE tests ADD COLUMN isShowAverage INTEGER DEFAULT 0').run()
+    // console.log('Колонка успешно добавлена')
+
+    // переименование колонки
+    // db.exec(`ALTER TABLE results RENAME COLUMN test_id TO testId;`)
+    // console.log('Колонка успешно переименована')
   }
   catch (err) {
-    if (err.message.includes('duplicate column name')) {
-      console.log('Column already exists, skipping...')
-    } else {
-      throw err
-    }
+    console.log(err)
   }
 }
 
